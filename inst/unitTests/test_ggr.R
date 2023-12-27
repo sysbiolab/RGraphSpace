04ee597b5c79cb2451f5f98342d73f85f9eca0bf
Mauro Castro <mauro.a.castro@gmail.com>
2023-12-27 14:10:01 -0300

    Minor update

--- inst/unitTests/test_ggr.R
+++ inst/unitTests/test_ggr.R
@@ -1,6 +1,6 @@
-# Unit tests fot GraphSpace
+# Unit tests fot RGraphSpace
 test_grs <- function(){
-  data("gtoy1", package = "GraphSpace")
+  data("gtoy1", package = "RGraphSpace")
   gs <- GraphSpace(gtoy1)
   checkTrue(is(gs, "GraphSpace"))
 }
